5daa8fadfdc2fa4fd26ac7c7ff0efd691b0be796
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2015-07-08 14:53:37 +0300

    Subparsers are now working

--- test_parser.py
+++ test_parser.py
@@ -15,6 +15,12 @@ subparsers = parser.add_subparsers(help = "My sub-command help")
 
 parser_a = subparsers.add_parser("Command a", help = "Help for a")
 parser_a.set_defaults(action = "Run a")
+subparsers_a = parser_a.add_subparsers(help = "My subsub-command help")
+parser_a_a = subparsers_a.add_parser("a_again", help = "Nested sub command")
+parser_a_a.set_defaults(action = "a_a")
+parser_a_b = subparsers_a.add_parser("b_again", help = "Another nested sub command")
+parser_a_b.set_defaults(action = "action")
+
 parser_b = subparsers.add_parser("Command b", help = "Help for b")
 parser_b.set_defaults(action = "Run b")
 
