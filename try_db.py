7961e96969c50957745181a4a60c1522d9b20646
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2015-07-08 14:46:01 +0300

    Add wishlist to try_db.py

--- try_db.py
+++ try_db.py
@@ -15,3 +15,40 @@ g.loadTable("totoGene")
 
 e = g.extractSimplifiedPeptides(0.10)
 g.writeSimplifiedPeptides("totoSimplifiedSeqs.fa")
+
+### * Wishlist
+
+script search "e.coli blabla" -o genbank-records/
+
+script tablify genbank-records/* --genes genes.table --records records.table
+script updateTable genes.table --hash md5
+script mergePeptides genes.table --maxDissimilarity 0.05 --mapping mergedPep.mapping
+
+script blastp mergedPep.mapping --ABC blastp.out.ABC --parallel 4
+script clusterMcl blastp.out.ABC --inflation 1.4 -o clusters.groups
+script buildAlignments genes.table --clusters clusters.groups --mapping mergedPep.mapping -o clusters/
+script refineAlignments
+
+### * Commands hierarchy
+
+- genbank
+  + search
+  + tablify
+
+- pyGene
+  + hash
+  + mergePeptides
+  
+- blastp
+  + convertBlastpABC
+
+- cluster
+  + convertBlastpABC
+  + mcl
+  + makeAlignments
+  + refineAlignments
+  + extendAlignments
+  
+- SNP
+  + call SNPs
+  
